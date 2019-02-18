b8919c8469243d2ba1e2af8b37c8137a521145d4
Chavi99 <44029954+Chavi99@users.noreply.github.com>
2019-02-18 05:37:46 -0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+def maximum_of_array(n,arr):
+    s=arr[0]
+    for i in arr:
+        if i<s:
+            s=i
+    print(s)
+n=int(input())
+ls=list(map(int,input().split()))
+maximum_of_array(n,ls)
